5d50d48580a39f8258ca4cbd86ded9b14650fe7a
gsvr <george@westerncapelabs.com>
2016-07-12 16:42:47 +0200

    add faq restriction to profile

--- casepro/profiles/context_processors.py
+++ casepro/profiles/context_processors.py
@@ -8,3 +8,13 @@ def user_is_admin(request):
     is_admin = request.org and not request.user.is_anonymous() and request.user.can_administer(request.org)
 
     return {'user_is_admin': is_admin}
+
+
+def user_must_reply_with_faq(request):
+    """
+    Context processor that adds boolean of whether current user must use a pre-approved reply
+    for responding to a message
+    """
+    must_use_faq = request.org and not request.user.is_anonymous() and request.user.must_use_faq()
+
+    return {'user_must_reply_with_faq': must_use_faq}
