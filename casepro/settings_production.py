27b70b23e6f961b3d7dc25a69b2539dc827a31ca
Simon de Haan <simon@praekelt.org>
2017-06-12 12:37:43 +0200

    backwards compatible settings construction

--- casepro/settings_production.py
+++ casepro/settings_production.py
@@ -54,16 +54,27 @@ def parse_channel_info(data):
         if not default_channel_id:
             default_channel_id = channel_id.strip()
         channels[channel_id.strip()] = {
-            'FROM_ADDR': from_addr.strip(),
+            'FROM_ADDRESS': from_addr.strip(),
             'API_ROOT': api_root.strip(),
         }
     return default_channel_id, channels
 
 
-inferred_default_channel, channel_info = parse_channel_info('JUNEBUG_CHANNELS', '')
-JUNEBUG_DEFAULT_CHANNEL_ID = os.environ.get(
-    'JUNEBUG_DEFAULT_CHANNEL_ID', inferred_default_channel)
-JUNEBUG_CHANNELS = channel_info
+if 'JUNEBUG_CHANNELS' in os.environ:
+    inferred_default_channel, channel_info = parse_channel_info('JUNEBUG_CHANNELS', '')
+    JUNEBUG_DEFAULT_CHANNEL_ID = os.environ.get(
+        'JUNEBUG_DEFAULT_CHANNEL_ID', inferred_default_channel)
+    JUNEBUG_CHANNELS = channel_info
+else:
+    # NOTE: allows graceful fallback to previous configuration style, pre reply-affinity
+    JUNEBUG_DEFAULT_CHANNEL_ID = os.environ.get('JUNEBUG_CHANNEL_ID', 'replace-me')
+    JUNEBUG_CHANNELS = {
+        JUNEBUG_DEFAULT_CHANNEL_ID: {
+            'FROM_ADDRESS': os.environ.get('JUNEBUG_FROM_ADDRESS', None),
+            'API_ROOT': os.environ.get('JUNEBUG_API_ROOT', 'http://localhost:8080/'),
+        }
+    }
+
 JUNEBUG_INBOUND_URL = r'^junebug/inbound$'
 JUNEBUG_HUB_BASE_URL = os.environ.get('JUNEBUG_HUB_BASE_URL', None)
 JUNEBUG_HUB_AUTH_TOKEN = os.environ.get('JUNEBUG_HUB_AUTH_TOKEN', None)
